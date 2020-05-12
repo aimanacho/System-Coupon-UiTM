c2502fa3153458c246cf63e511f8e80464adeb00
Aiman Mohd Said <37135470+aimanacho@users.noreply.github.com>
2020-05-13 00:43:27 +0800

    Update accEvent.php

--- accEvent.php
+++ accEvent.php
@@ -20,7 +20,7 @@ if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
   <body>
     <!-- topbar-->
     <ul class="topnav" id= "main">
-      <li class="right"><a href="index.php">Logout</a></li>
+      <li class="right"><a href="index.php">Logoutssss</a></li>
     </ul>
 
     <!-- sidebar-->
