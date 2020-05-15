8e34fca3eb3293d1873db2c3f2578aac58a3fba7
aimanacho <warizupguys@hotmail.com>
2020-05-16 00:33:08 +0800

    settle bila tekan one of the student kelaur full student information xD

--- student.php
+++ student.php
@@ -2,59 +2,96 @@
 session_start();
 if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
 {
-header("Location: index.php");
+  header("Location: index.php");
 }
-?>
+ ?>
 
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
-  <link rel="stylesheet" href= "styledashboardsidebar.css"/>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-  <!-- topbar-->
-  <ul class="topnav" id= "main">
-    <li class="right"><a href="index.php">Logout</a></li>
-  </ul>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <link rel="stylesheet" href= "styledashboardsidebar.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <!-- topbar-->
+    <ul class="topnav" id= "main">
+      <li class="right"><a href="index.php">Logout</a></li>
+    </ul>
 
-  <!-- sidebar-->
-<div class="sidenav">
-  <img src = "uitm.jpg"/>
-  <a href="dashboard.php" class = "btn"> Dashboard</a>
-  <a href="clubs.php" class = "btn">Clubs</a>
-  <a href="studentinfo.php" class = "btn active">Student Info</a>
-  <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
-    <i class = "fa fa-caret-down"></i>
-  </a>
-  <div class = "dropdown-container" >
-    <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">View events</a>
-    <a href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
+    <!-- sidebar-->
+  <div class="sidenav">
+    <img src = "uitm.jpg"/>
+    <a href="dashboard.php" class = "btn"> Dashboard</a>
+    <a href="clubs.php" class = "btn">Clubs</a>
+    <a href="studentinfo.php" class = "btn active">Student Info</a>
+    <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
+      <i class = "fa fa-caret-down"></i>
+    </a>
+    <div class = "dropdown-container" >
+      <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">View events</a>
+      <a href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
+    </div>
+    <a href="report.php" class = "btn">Report</a>
   </div>
-  <a href="report.php" class = "btn">Report</a>
-</div>
 
-<!-- content -->
-<div class = "content">
-  <form action = "studentinfo.php" style = "text-align: center;">
-    <button type="submit" class="btn btn-primary"> Back</button>
-  </form>
-  <p style = "text-align: center;font-size: 30px;"><b>Student Information</b></p><br>
-  <?php
-    include ("connection.php");
-    $matricNo =  $_POST['matricNo'];
-    $sql = "SELECT * from student WHERE matricNo = '".$matricNo."'";
-    $result = mysqli_query($conn, $sql);
-      if ($row = mysqli_fetch_assoc($result))
+  <!-- content -->
+  <div class = "content">
+    <form action = "studentinfo.php" style = "text-align: center;">
+      <button type="submit" class="btn btn-primary"> Back</button>
+    </form>
+    <p style = "text-align: center;font-size: 30px;"><b>Student information</b></p><br>
+    <?php
+      include ("connection.php");
+      $matricNo = $_POST['matricNo'];
+      $sql = "SELECT * from student WHERE matricNo = '".$matricNo."'";
+      $result = mysqli_query($conn, $sql);
+        if ($row = mysqli_fetch_assoc($result))
+        {
+          echo "<p> ID:".$row["matricNo"]."</p>";
+          echo "<p> Name:".$row["studentname"]."</p>";
+          echo "<p> Semester:".$row["sem"]."</p>";
+          echo "<p> Total Merit:".$row["merit"]."</p>";
+        }
+      ?>
+
+
+ </div>
+
+  <!-- script -->
+<script>
+  /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+  var dropdown = document.getElementsByClassName("dropdown-btn");
+  var i;
+
+  for (i = 0; i < dropdown.length; i++)
+  {
+    dropdown[i].addEventListener("click", function()
+    {
+      this.classList.toggle("active");
+      var dropdownContent = this.nextElementSibling;
+      if (dropdownContent.style.display === "block")
       {
-        echo "<p> Student Name:".$row["studentname"]."</p>";
-        echo "<p> Matric No:".$row["matricNo"]."</p>";
-        echo "<p> Semester:".$row["sem"]."</p>";
-        echo "<p> Total Merit:".$row["merit"]."</p>";
+        dropdownContent.style.display = "none";
       }
-    ?>
+      else
+      {
+        dropdownContent.style.display = "block";
+      }
+      });
+  }
+
+    $("button").click(function() {
+      var fired_button = $(this).val();
+      alert(fired_button);
+  });
+</script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+  </body>
+</html>
--- studentinfo.php
+++ studentinfo.php
@@ -2,113 +2,110 @@
 session_start();
 if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
 {
-  header("Location: index.php");
+header("Location: index.php");
 }
- ?>
+?>
 
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title></title>
-    <link rel="stylesheet" href= "styledashboardsidebar.css"/>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  </head>
-  <body>
-    <!-- topbar-->
-    <ul class="topnav" id= "main">
-      <li class="right"><a href="index.php">Logout</a></li>
-    </ul>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title></title>
+  <link rel="stylesheet" href= "styledashboardsidebar.css"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <!-- topbar-->
+  <ul class="topnav" id= "main">
+    <li class="right"><a href="index.php">Logout</a></li>
+  </ul>
 
-    <!-- sidebar-->
-  <div class="sidenav">
-    <img src = "uitm.jpg"/>
-    <a href="dashboard.php" class = "btn"> Dashboard</a>
-    <a href="clubs.php" class = "btn">Clubs</a>
-    <a href="studentinfo.php" class = "btn active">Student Info</a>
-    <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
-      <i class = "fa fa-caret-down"></i>
-    </a>
-    <div class = "dropdown-container" >
-      <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">View events</a>
-      <a href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
-    </div>
-    <a href="report.php" class = "btn">Report</a>
+  <!-- sidebar-->
+<div class="sidenav">
+  <img src = "uitm.jpg"/>
+  <a href="dashboard.php" class = "btn"> Dashboard</a>
+  <a href="clubs.php" class = "btn">Clubs</a>
+  <a href="studentinfo.php" class = "btn active">Student Info</a>
+  <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
+    <i class = "fa fa-caret-down"></i>
+  </a>
+  <div class = "dropdown-container" >
+    <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">View events</a>
+    <a href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
   </div>
+  <a href="report.php" class = "btn">Report</a>
+</div>
 
-  <!-- content -->
-  <p class = "content"><b>All student information</b></p>
+<!-- content -->
+<p class = "content"><b>Student Info</b></p>
 
 
-  <!-- table -->
-  <table class="table table-bordered" id= "tablemeow">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Semester</th>
-        <th>Total Merit</th>
-        <th>More Details</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      include("connection.php");
-      $sql = "SELECT * from student ORDER BY studentname";
-      $result = mysqli_query($conn, $sql);
+<!-- table -->
+<table class="table table-bordered" id= "tablemeow">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Semester</th>
+      <th>Total Merit</th>
+      <th>More Details</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    include("connection.php");
+    $sql = "SELECT * from student ORDER BY studentname";
+    $result = mysqli_query($conn, $sql);
       while ($row = mysqli_fetch_assoc($result))
       {
-        $_SESSION['matricNo'] = $row['matricNo'];
-        echo "<form method = post action = student.php>";
-        echo
-          "<tr>
-            <td>".$row["matricNo"]."</td>
-            <td>".$row["studentname"]."</td>
-            <td>".$row["sem"]."</td>
-            <td>".$row["merit"]."</td>
-            <input type = hidden name = matricNo value = ".$_SESSION['matricNo']." />
-            <td><button>Details</button></td>
-          </tr>
-        <form/>";
+        echo "<form action = student.php method = post >";
+        echo "<tr>";
+        echo "<td>".$row["matricNo"]."</td>";
+        echo "<td>".$row["studentname"]."</td>";
+        echo "<td>".$row["sem"]."</td>";
+        echo "<td>".$row["merit"]."</td>";
+        echo "<input type = 'hidden' name = 'matricNo' value = '".$row['matricNo']."' />";
+        echo "<td><button>Enter</button></td>";
+        echo "</tr>";
+        echo "</form>";
       }
       ?>
-    </tbody>
-  </table>
+  </tbody>
+</table>
 
 <!-- script -->
 <script>
-  /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-  var dropdown = document.getElementsByClassName("dropdown-btn");
-  var i;
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
 
-  for (i = 0; i < dropdown.length; i++)
+for (i = 0; i < dropdown.length; i++)
+{
+  dropdown[i].addEventListener("click", function()
   {
-    dropdown[i].addEventListener("click", function()
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block")
     {
-      this.classList.toggle("active");
-      var dropdownContent = this.nextElementSibling;
-      if (dropdownContent.style.display === "block")
-      {
-        dropdownContent.style.display = "none";
-      }
-      else
-      {
-        dropdownContent.style.display = "block";
-      }
-      });
-  }
+      dropdownContent.style.display = "none";
+    }
+    else
+    {
+      dropdownContent.style.display = "block";
+    }
+    });
+}
 
-  $("button").click(function() {
-    var fired_button = $(this).val();
-    alert(fired_button);
+$("button").click(function() {
+  var fired_button = $(this).val();
+  alert(fired_button);
 });
-
 </script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
-  </body>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
+</body>
 </html>
