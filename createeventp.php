2e0abf1199ca166c4a44abe1de11395144656ae7
aimanacho <warizupguys@hotmail.com>
2020-05-16 15:13:36 +0800

    latest send email prcoess (transfer form.php to createventp.php)

--- createeventp.php
+++ createeventp.php
@@ -17,5 +17,22 @@
     ('".$eventname."', '".$eventvenue."','".$eventdate."','".$timestart."','".$timeend."','".$merit."','".$couponq."','".$eventstatus."', '".$clubCode."')";
 
     mysqli_query($conn, $sql);
+    require_once('PHPMailerAutoload.php');
+
+    $mail = new PHPMailer();
+    $mail ->isSMTP();
+    $mail ->SMTPAuth = true;
+    $mail ->SMTPSecure = 'ssl';
+    $mail ->Host = 'smtp.gmail.com';
+    $mail ->Port = '465';
+    $mail ->isHTML();
+    $mail ->Username = 'aimanachotest@gmail.com';
+    $mail ->Password = 'test1201';
+    $mail ->SetFrom('aimanachotest@gmail.com');
+    $mail ->Subject = 'A new submission from student';
+    $mail ->Body = ' '.$clubCode.' has created program! Please check our website.';
+    $mail ->AddAddress('aimanachotest@gmail.com');
+
+    $mail->Send();
     echo "<script language = 'javascript'>alert('Registration is success.');window.location='dashboard.php';</script>";
  ?>
