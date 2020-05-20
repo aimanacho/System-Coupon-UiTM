7bbaeafda183a3a9069cbbe3de46ad6dd73eaadc
aimanacho <warizupguys@hotmail.com>
2020-05-20 22:07:11 +0800

    asd

--- clubs.php
+++ clubs.php
@@ -14,6 +14,7 @@ if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title></title>
     <link rel="stylesheet" href= "styledashboardsidebar.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
