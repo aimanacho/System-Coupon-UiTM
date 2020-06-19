ae99d6e28542e62d608dfa97a7ded9325c4c508e
aimanacho <warizupguys@hotmail.com>
2020-06-19 23:59:05 +0800

    asd

---
+++ bar.php
@@ -0,0 +1,40 @@
+<!-- topbar-->
+<ul class="topnav" id= "main">
+  <li class="right"><a href="logout.php">Logout</a></li>
+</ul>
+
+<!-- sidebar-->
+<!-- dem-->
+<?php if ($_SESSION['userlevelid']== 1){ ?>
+  <div class="sidenav" id = "myDIV">
+    <img src = "uitm.jpg"/>
+    <a href="dashboard.php" class = "btn active"> Dashboard</a>
+    <a href="attendance.php" class = "btn">Attendance</a>
+    <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
+      <i class = "fa fa-caret-down"></i>
+    </a>
+    <div class = "dropdown-container" >
+      <a href= "createevent.php" class= "btn" style= "text-align: left;font-size: 18px;">Create events</a>
+      <a href= "vieweventdem.php" class= "btn" style= "text-align: left;font-size: 18px;">View events</a>
+    </div>
+  </div>
+  <?php } ?>
+
+<!-- hep-->
+<?php if ($_SESSION['userlevelid']== 2){ ?>
+<div class="sidenav" id = "myDIV">
+  <img src = "uitm.jpg"/>
+  <a href="dashboard.php" class = "btn active"> Dashboard</a>
+  <a href="clubs.php" class = "btn">Clubs</a>
+  <a href="studentinfo.php" class = "btn">Student Info</a>
+  <a class= "dropdown-btn btn" style = "font-size: 25px;">Events
+    <i class = "fa fa-caret-down"></i>
+  </a>
+  <div class = "dropdown-container" >
+    <a class = "btn" href= "viewevent.php" style= "text-align: left;font-size: 18px;">Upcoming events</a>
+    <a class = "btn" href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
+    <a class = "btn" href= "historyevent.php" style= "text-align: left;font-size: 18px;">History events</a>
+  </div>
+  <a href="report.php" class = "btn">Report</a>
+</div>
+<?php } ?>
