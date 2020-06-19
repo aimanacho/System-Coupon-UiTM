3bd9c74eea0fb0480fcd8228e0c7c2a45176aaf4
Aiman Mohd Said <aimanmohdsaid@Aimans-MacBook-Pro.local>
2020-06-19 15:37:58 +0800

    asd

--- heplogin.php
+++ heplogin.php
@@ -7,7 +7,9 @@
     <title>System Coupon UiTM</title>
     <link rel="stylesheet" href= "stylelogin.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body style = "background-color: rgb(163, 194, 194);">
     <!-- table  -->
@@ -16,12 +18,16 @@
         <p style="font-size:30px;"> HEP LOGIN </p>
         <form action = "pheplogin.php" method = "post" id = "login" name = "login" target = "_self">
           <div>
-            <label >Username</label><br/>
-            <input style = "border-color: black;margin-left:12px;" type="text" id="studno" name="studno" placeholder="Student ID"><br>
-            <label >Password</label><br/>
-            <input style = "border-color: black;margin-left:12px;" type="password" id="password" name="password" placeholder="Password"><br><br>
+            <div class="input-container">
+              <i class="fa fa-user icon"></i>
+              <input class="input-field" type="text" id="studno" name="studno" placeholder="Student ID" >
+            </div>
+            <div class="input-container">
+              <i class="fa fa-key icon"></i>
+              <input class="input-field" type="password" placeholder="Password" name="psw">
+            </div>
             <input type="submit" value="Login" class="btn btn-primary">
-            <input type="submit" formaction="index.php" value="Back" class="btn btn-default">
+            <input type="submit" formaction="index.php" value="Back" class="btn btn-info">
           </div>
         </form>
           <div class="alert alert-danger" style="margin-top:50px;width:230px;">
