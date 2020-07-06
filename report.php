18219f7ff9de0bd11bae54096c974c0df1a9142d
permataaz <permataazman@gmail.com>
2020-07-06 22:51:20 +0800

    add button pdf

--- report.php
+++ report.php
@@ -33,13 +33,52 @@ if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
       <i class = "fa fa-caret-down"></i>
     </a>
     <div class = "dropdown-container" >
-      <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">View events</a>
+      <a href= "viewevent.php" style= "text-align: left;font-size: 18px;">Upcoming events</a>
       <a href= "pendingevent.php" style= "text-align: left;font-size: 18px;">Pending events</a>
+      <a class = "btn" href= "historyevent.php" style= "text-align: left;font-size: 18px;">History events</a>
     </div>
     <a href="report.php" class = "btn active">Report</a>
   </div>
 
+<!--bruh idk what to put here-->
+<!-- content -->
+<p class = "content"><b>Qualification for College Placement</b></p>
 
+
+<!-- table -->
+<table class="table table-bordered" id= "tablemeow">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Semester</th>
+      <th>Total Coupons</th>
+      <th>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+    include("connection.php");
+    $sql = "SELECT student.matricNo, student.studentname, student.sem, sum(events.meritE) as totalmerit from student join attendance on student.matricNo=attendance.matricno join events on events.eventcode = attendance.eventcode Group by student.matricNo ORDER BY student.matricNo, student.studentname, student.sem";
+    $result = mysqli_query($conn, $sql);
+      while ($row = mysqli_fetch_assoc($result))
+      {
+        echo "<form action = student.php method = post >";
+        echo "<tr>";
+        echo "<td>".$row["matricNo"]."</td>";
+        echo "<td>".$row["studentname"]."</td>";
+        echo "<td>".$row["sem"]."</td>";
+        echo "<td>".$row["totalmerit"]."</td>";
+
+    // echo "<input type = 'hidden' name = 'matricNo' value = '".$row['matricNo']."' />";
+        //echo "<td>".$row[""]."</td>"; <-- tambah sql statement for sum (join table)
+      // echo "<td><button>Enter</button></td>";
+        echo "</tr>";
+        echo "</form>";
+      }
+      ?>
+  </tbody>
+</table>
 <!-- script -->
 <script>
   /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
@@ -62,7 +101,41 @@ if ( !isset($_SESSION['userlogged']) || $_SESSION['userlogged'] != 1)
       }
       });
   }
+
 </script>
+
+//<!--DOCTYPE html-->
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.btn {
+  background-color: Grey;
+  border: none;
+  color: white;
+  padding: 12px 30px;
+  cursor: pointer;
+  font-size: 20px;
+}
+
+/* Darker background on mouse-over */
+.btn:hover {
+  background-color: Darkgrey;
+}
+</style>
+</head>
+<body>
+  <form action = "testpdf.php" style = "text-align: center;">
+    <button type="submit" class="btn btn-primary">Print PDF</button>
+  </form>
+
+
+    </div>
+ </div>
+
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/js/bootstrap.min.js" integrity="sha384-vZ2WRJMwsjRMW/8U7i6PWi6AlO1L79snBrmgiDpgIWJ82z8eA5lenwvxbMV1PAh7" crossorigin="anonymous"></script>
