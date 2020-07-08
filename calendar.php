e8e92d1ce5ec91d0a9c73c7c1c4eee1a704e349d
Aiman Mohd Said <aimanmohdsaid@Aimans-MacBook-Pro.local>
2020-07-08 22:01:52 +0800

    asd

--- calendar.php
+++ calendar.php
@@ -8,7 +8,7 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>Jquery Fullcalendar Integration with PHP and Mysql</title>
+  <title>Event Calendar</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -20,7 +20,6 @@
     {
       var calendar = $('#calendar').fullCalendar(
         {
-          editable:true,
           header:
           {
             left:'prev,next,today',
@@ -28,6 +27,9 @@
             right: 'month,agendaWeek,agendaDay'
           },
           events:'calendarload.php',
+          selectable:true,
+          selectHelper:true,
+          eventLimit: true, // allow "more" link when too many events
         });
     });
   </script>
